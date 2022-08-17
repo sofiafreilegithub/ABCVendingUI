cdcf16d9ef2c040108451d57587e088f8f242a68
sofiafreilegithub <110150547+sofiafreilegithub@users.noreply.github.com>
2022-08-17 10:13:24 -0400

    Update inventory.component.spec.ts

--- Warehouse-app/src/app/inventory/inventory.component.spec.ts
+++ Warehouse-app/src/app/inventory/inventory.component.spec.ts
@@ -1,26 +1,48 @@
-//import { ApiService } from 'src/app/api-service';
+import { ApiService } from 'src/app/api-service';
 import { doesNotThrow } from 'assert';
-import { of } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { InventoryComponent } from './inventory.component'
+import { Product } from './inventory.interface'
 
 describe('InventoryComponent', ()=> {
   let mockApiService : any;
   let component : InventoryComponent;
+  const mockProduct: Product = {
+    WarehouseId: 1,
+    warehouseName: 'MockWarehouseName',
+    ProductCategoryId: 1,
+    productCategoryName: 'MockCategoryName',
+    ProductId: 1,
+    productName: 'MockProductName',
+    productCount: 10,
+    LocationId: 1
+  }
+  let mockProductArray : [Product] = [mockProduct];
+  
   beforeEach(() => {
     mockApiService = {
         updateInventory: jasmine.createSpy('mockApiService.updateInventory'),
-        inventoryData$: jasmine.createSpy('mockApiService.inventoryData$')
+        makeRequest: jasmine.createSpy('mockApiService.makeRequest'),
     };
+    mockApiService.inventoryData$ = of(mockProductArray);
+    mockApiService.inventoryTotals$ = of(mockProductArray);
     component = new InventoryComponent(mockApiService);
-    component.updateInventory();
+    component.initialize();
+    component.updateInventory();    
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  }); 
-
   describe('get inventory data', () => {
-    it('updates inventory$', () => {
-      expect(component.inventory$).toBeDefined();
+    it('update inventory', (done) => {
+      expect(component.totals$).toBeDefined();
+      
+      component.totals$.subscribe((value) => {
+        const p: Product = value.find((v : Product)=> v.productCategoryName ='MockCategoryName');
+        console.log(p);
+        expect(p.productCategoryName)
+          .toEqual('MockCategoryName');
+        done();
+      });
+
+      expect(component.categoryTotals[0].total).toEqual(10);
   })})
 })
